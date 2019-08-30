fbc41e54465d24b7b7765293a36a95670eaa2622
Michael Reuter <mareuter@lsst.org>
2019-08-30 15:39:33 -0700

    Add time conversion tests around leap seconds.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,7 +8,18 @@ class TestUtils(unittest.TestCase):
     def test_convert_time(self):
         time_string = '2019-08-08T22:26:52.451723'
         tai_time_stamp = 1565303249.451723
+        self.assertEqual(utils.convert_time(time_string), tai_time_stamp)
 
+        time_string = '2016-12-31T23:59:59.951723'
+        tai_time_stamp = 1483228835.951723
+        self.assertEqual(utils.convert_time(time_string), tai_time_stamp)
+
+        time_string = '2016-12-31T23:59:60.351723'
+        tai_time_stamp = 1483228836.351723
+        self.assertEqual(utils.convert_time(time_string), tai_time_stamp)
+
+        time_string = '2017-01-01T00:00:00.151723'
+        tai_time_stamp = 1483228837.151723
         self.assertEqual(utils.convert_time(time_string), tai_time_stamp)
 
 

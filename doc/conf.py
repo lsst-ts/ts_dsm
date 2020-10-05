fdc74afe098583e8c60e8f972fc3a0c8fe86a2a8
Michael Reuter <mareuter@lsst.org>
2020-10-05 08:45:57 -0700

    Removing old docs and adding new pages.

--- doc/conf.py
+++ doc/conf.py
@@ -1,15 +1,13 @@
-"""Sphinx configuration file for an LSST stack package.
-
-This configuration only affects single-package Sphinx documentation builds.
-"""
+"""Sphinx configuration file for TSSW package"""
 
 from documenteer.sphinxconfig.stackconf import build_package_configs
 import lsst.ts.dsm
 
 
 _g = globals()
-_g.update(
-    build_package_configs(
-        project_name="ts_dsm", version=lsst.ts.dsm.version.__version__
-    )
-)
+_g.update(build_package_configs(
+    project_name='ts_dsm',
+    version=lsst.ts.dsm.version.__version__
+))
+
+intersphinx_mapping["ts_xml"] = ("https://ts-xml.lsst.io", None)

40b2d6785011780d5c501110c0d643281e9c4c0f
Michael Reuter <mareuter@lsst.org>
2020-10-12 17:15:46 -0700

    Add provisions for black formatting/testing.

--- doc/conf.py
+++ doc/conf.py
@@ -5,9 +5,10 @@ import lsst.ts.dsm
 
 
 _g = globals()
-_g.update(build_package_configs(
-    project_name='ts_dsm',
-    version=lsst.ts.dsm.version.__version__
-))
+_g.update(
+    build_package_configs(
+        project_name="ts_dsm", version=lsst.ts.dsm.version.__version__
+    )
+)
 
 intersphinx_mapping["ts_xml"] = ("https://ts-xml.lsst.io", None)

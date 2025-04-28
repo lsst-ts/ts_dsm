1b28bcf922d2281a49c41c2cdeb3bb6404a7cc23
Michael Reuter <mareuter@lsst.org>
2025-04-28 11:29:10 -0700

    Trim setup.py.

--- setup.py
+++ setup.py
@@ -5,14 +5,6 @@ import setuptools_scm
 
 scm_version = metadata.version("setuptools_scm")
 
-if scm_version.startswith("8"):
-    setuptools.setup(
-        version=setuptools_scm.get_version(
-            version_file="python/lsst/ts/dsm/version.py",
-            relative_to="pyproject.toml",
-        )
-    )
-else:
-    setuptools.setup(
-        version=setuptools_scm.get_version(write_to="python/lsst/ts/dsm/version.py")
-    )
+setuptools.setup(
+    version=setuptools_scm.get_version(write_to="python/lsst/ts/dsm/version.py")
+)
